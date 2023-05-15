dc4b3f44a44c67280f8afa54b16770afc0b5de2e
azatprojazz <azatprojazz@gmail.com>
2023-05-15 21:45:11 +0300

    fix: рефакторинг кода

--- backend/middlewares/corsRequest.js
+++ backend/middlewares/corsRequest.js
@@ -26,9 +26,10 @@ const corsRequest = (req, res, next) => {
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
+    res.end();
+    return;
   }
-  return next();
+  next();
 };
 
 module.exports = corsRequest;
